706956accb9a54ee76f0f4bd77831be9625b9cdc
Atul Yadav <atul99yadav99@gmail.com>
2026-04-24 10:13:28 +0530

    ai remove

--- backend/src/controllers/imagetransfr.controller.js
+++ backend/src/controllers/imagetransfr.controller.js
@@ -2,23 +2,59 @@ import sharp from "sharp";
 import asyncHandler from "../utils/asyncHandler.js";
 import errorhandler from "../utils/errorhandler.js";
 import responseHandler from "../utils/responseHandler.js";
-import {s3, getFileFromS3, uploadonawss3bucket } from "../utils/uploadonawss3bucket.js";
+import {
+  s3,
+  getFileFromS3,
+  uploadonawss3bucket,
+} from "../utils/uploadonawss3bucket.js";
 import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
-const imagetransf=asyncHandler(async(req,res)=>{
-    const {width,height,rotate,blur,format}=req.query;
-    const fileobject=await s3.send(new GetObjectCommand({
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: req.params.key,}));
-    let pipeline=sharp();
-    if(width&&height) pipeline=pipeline.resize({width:Number(width),height:Number(height)});
-    else if(width) pipeline=pipeline.resize({width:Number(width)});
-    else if(height) pipeline=pipeline.resize({height:Number(height)});
-   
-    if(rotate) pipeline=pipeline.rotate(Number(rotate));
+import { removeBg } from "../utils/backgroundai.js";
 
-      if(blur) pipeline=pipeline.blur(Number(blur));
-    if(format) pipeline=pipeline.toFormat(format);
-    res.setHeader("Content-Type", `image/${format||"jpeg"}`);
-    fileobject.Body.pipe(pipeline).pipe(res)
-})
-export {imagetransf};
+import {pipeline} from "stream/promises";
+
+const imagetransf = asyncHandler(async (req, res) => {
+  const { removebg, width, height, rotate, blur, format } = req.query;
+  if (removebg === "true") { 
+    const pathtofile = await getFileFromS3(req.params.key); //get path of file from s3
+    const removebgresponse = await removeBg(pathtofile);
+   
+    let picpipeline = sharp();
+    if (width && height)
+      picpipeline = picpipeline.resize({
+        width: Number(width),
+        height: Number(height),
+      });
+    else if (width) picpipeline = picpipeline.resize({ width: Number(width) });
+    else if (height) picpipeline = picpipeline.resize({ height: Number(height) });
+
+    if (rotate) picpipeline = picpipeline.rotate(Number(rotate));
+
+    if (blur) picpipeline = picpipeline.blur(Number(blur));
+    if (format) picpipeline = picpipeline.toFormat(format);
+    res.setHeader("Content-Type", `image/${format || "jpeg"}`);
+   await pipeline(removebgresponse,picpipeline,res);
+  } else {
+    const fileobject = await s3.send(
+      new GetObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: req.params.key,
+      }),
+    );
+    let picpipeline = sharp();
+    if (width && height)
+      picpipeline = picpipeline.resize({
+        width: Number(width),
+        height: Number(height),
+      });
+    else if (width) picpipeline = picpipeline.resize({ width: Number(width) });
+    else if (height) picpipeline = picpipeline.resize({ height: Number(height) });
+
+    if (rotate) picpipeline = picpipeline.rotate(Number(rotate));
+
+    if (blur) picpipeline = picpipeline.blur(Number(blur));
+    if (format) picpipeline = picpipeline.toFormat(format);
+    res.setHeader("Content-Type", `image/${format || "jpeg"}`);
+    fileobject.Body.pipe(picpipeline).pipe(res);
+  }
+});
+export { imagetransf };
--- backend/src/utils/backgroundai.js
+++ backend/src/utils/backgroundai.js
@@ -9,12 +9,12 @@ const removeBg = async(imageURL) => {
 
   const response = await fetch("https://api.remove.bg/v1.0/removebg", {
     method: "POST",
-    headers: { "X-Api-Key": "INSERT_YOUR_API_KEY_HERE" },
+    headers: { "X-Api-Key": process.env.REMOVEBG_API_KEY },
     body: formData,
   });
 
   if (response.ok) {
-    return await response.arrayBuffer();
+    return await response.body;
   } else {
     throw new errorhandler(`${response.status}: ${response.statusText}`);
   }
