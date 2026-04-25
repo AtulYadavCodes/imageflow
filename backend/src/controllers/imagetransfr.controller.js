8a724f4e9efedac12d0b9482fc04927f58d1d066
Atul Yadav <atul99yadav99@gmail.com>
2026-04-25 11:55:20 +0530

    fit error handle

--- backend/src/controllers/imagetransfr.controller.js
+++ backend/src/controllers/imagetransfr.controller.js
@@ -1,5 +1,5 @@
 import sharp from "sharp";
-import {asyncHandler} from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import errorhandler from "../utils/errorhandler.js";
 import responseHandler from "../utils/responseHandler.js";
 import {
@@ -10,38 +10,67 @@ import {
 import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import { removeBg } from "../utils/backgroundai.js";
 
-import {pipeline} from "stream/promises";
+import { pipeline } from "stream/promises";
 
 const imagetransf = asyncHandler(async (req, res) => {
-  const { removebg,gray,fit="cover", width, height, rotate, blur, format } = req.query;
-  if(width && isNaN(width)) throw new errorhandler(400, "Width must be a number", []);
-  if(height && isNaN(height)) throw new errorhandler(400, "Height must be a number", []);
-  if(rotate && isNaN(rotate)) throw new errorhandler(400, "Rotate must be a number", []);
-  if(blur && isNaN(blur)) throw new errorhandler(400, "Blur must be a number", []);
+  const {
+    removebg,
+    gray,
+    fit = "cover",
+    width,
+    height,
+    rotate,
+    blur,
+    format,
+  } = req.query;
+  if (width && isNaN(width))
+    throw new errorhandler(400, "Width must be a number", []);
+  if (height && isNaN(height))
+    throw new errorhandler(400, "Height must be a number", []);
+  if (rotate && isNaN(rotate))
+    throw new errorhandler(400, "Rotate must be a number", []);
+  if (blur && isNaN(blur))
+    throw new errorhandler(400, "Blur must be a number", []);
 
-  if(format && !["jpeg", "png", "webp", "tiff", "avif"].includes(format.toLowerCase())){
-    throw new errorhandler(400, "Invalid format. Supported formats are jpeg, png, webp, tiff, avif", []);
-}
-  if (removebg === "true") { 
+  if (
+    format &&
+    !["jpeg", "png", "webp", "tiff", "avif"].includes(format.toLowerCase())
+  ) {
+    throw new errorhandler(
+      400,
+      "Invalid format. Supported formats are jpeg, png, webp, tiff, avif",
+      [],
+    );
+  }
+  if(fit && !["cover", "contain", "fill", "inside", "outside"].includes(fit.toLowerCase()))
+  {    throw new errorhandler(
+    400,
+    "Invalid fit. Supported fits are cover, contain, fill, inside, outside",
+    [],
+  );}
+
+  if (removebg === "true") {
     const pathtofile = await getFileFromS3(req.params.key.join("/")); //get path of file from s3
-    const removebgresponse = await removeBg(pathtofile); 
-   
+    const removebgresponse = await removeBg(pathtofile);
+
     let picpipeline = sharp();
-    if(gray=="true") picpipeline=picpipeline.greyscale();
+    if (gray == "true") picpipeline = picpipeline.greyscale();
     if (width && height)
       picpipeline = picpipeline.resize({
         width: Number(width),
-        height: Number(height),fit:fit
+        height: Number(height),
+        fit: fit,
       });
     else if (width) picpipeline = picpipeline.resize({ width: Number(width) });
-    else if (height) picpipeline = picpipeline.resize({ height: Number(height) });
+    else if (height)
+      picpipeline = picpipeline.resize({ height: Number(height) });
 
     if (rotate) picpipeline = picpipeline.rotate(Number(rotate));
 
     if (blur) picpipeline = picpipeline.blur(Number(blur));
     if (format) picpipeline = picpipeline.toFormat(format);
     res.setHeader("Content-Type", `image/${format || "jpeg"}`);
-   await pipeline(removebgresponse,picpipeline,res);
+    await pipeline(removebgresponse, picpipeline, res);
   } else {
     const fileobject = await s3.send(
       new GetObjectCommand({
@@ -51,15 +80,16 @@ const imagetransf = asyncHandler(async (req, res) => {
     );
     let picpipeline = sharp();
 
-   
-    if(gray=="true") picpipeline=picpipeline.greyscale();
+    if (gray == "true") picpipeline = picpipeline.greyscale();
     if (width && height)
       picpipeline = picpipeline.resize({
         width: Number(width),
-        height: Number(height),fit:fit
+        height: Number(height),
+        fit: fit,
       });
     else if (width) picpipeline = picpipeline.resize({ width: Number(width) });
-    else if (height) picpipeline = picpipeline.resize({ height: Number(height) });
+    else if (height)
+      picpipeline = picpipeline.resize({ height: Number(height) });
 
     if (rotate) picpipeline = picpipeline.rotate(Number(rotate));
 
