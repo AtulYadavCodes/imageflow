7fa34acd1dc2036d77f1a1828172ef5afdbaf028
Atul Yadav <atul99yadav99@gmail.com>
2026-04-26 22:51:07 +0530

    user cant be used by api

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import {Router} from "express"
 import { upload } from "../middlewares/multer.middleware.js"
 import { loginuser, logoutuser, registerUser,refreshAccessToken, returnuserProfile,updateuseravatar,updateuserpassword,updateuseremail} from "../controllers/user.controller.js"
-import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { verifyJWT,requireJwtAuth } from "../middlewares/auth.middleware.js"
 
 import { ratelimMiddleware } from "../middlewares/ratelim.middleware.js"
 const router=Router()
@@ -12,10 +12,10 @@ router.route('/login').post(upload.none(),ratelimMiddleware, loginuser)
 
 
 //secured routes 
-router.route('/logout').post(verifyJWT,logoutuser);
+router.route('/logout').post(verifyJWT,requireJwtAuth,logoutuser);
 router.route('/refreshAccessToken').post(refreshAccessToken);
-router.route('/profile').get(verifyJWT,returnuserProfile);
-router.route('/updateprofileavatar').patch(verifyJWT,upload.single('avatar'),updateuseravatar);
-router.route('/updatepassword').post(verifyJWT,upload.none(),updateuserpassword);
-router.route('/updateemail').patch(verifyJWT,upload.none(),updateuseremail);
+router.route('/profile').get(verifyJWT,requireJwtAuth,returnuserProfile);
+router.route('/updateprofileavatar').patch(verifyJWT,requireJwtAuth,upload.single('avatar'),updateuseravatar);
+router.route('/updatepassword').post(verifyJWT,requireJwtAuth,upload.none(),updateuserpassword);
+router.route('/updateemail').patch(verifyJWT,requireJwtAuth,upload.none(),updateuseremail);
 export default router
