4be780fb99429460e84a21698bc47b2d3d36cb61
Atul Yadav <atul99yadav99@gmail.com>
2026-04-28 11:43:34 +0530

    wrap context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './Context/LoginContext.jsx';//wrapping the entire app in context provider so that we can access the auth state from anywhere in the app
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
