f0fcf30e1ef8e3d6e3b540c0c817cc03accde540
Atul Yadav <atul99yadav99@gmail.com>
2026-04-28 11:41:49 +0530

    custom fucnton

--- frontend/src/components/Router/Router.jsx
+++ frontend/src/components/Router/Router.jsx
@@ -3,16 +3,37 @@ import Layout from "../layout/Layout";
 import Home from "../pages/HomePage";
 import ApiDocPage from "../pages/ApiDocPage";
 
-import React from 'react'
+import AuthPage from "../pages/AuthPage";
 
+
+
+
+import DashboardPage from "../pages/DashboardPage";
+
+import React from 'react'
+import ProtectedRoute from "./ProtectedRoute";
+
+
+
+import { useAuth } from "../../Context/LoginContext";
+const blockroutefromloggedinuser=({children})=>{
+  const {isAuth} = useAuth();
+  if(isAuth) return <Navigate to="/dashboard" replace />
+  return children;
+}
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
         children: [
             {path: "", element: <Home />},
-            {path: "apidocs", element: <ApiDocPage />}
+            {path: "apidocs", element: <ApiDocPage />},
+            {path:"auth",element: <blockroutefromloggedinuser><AuthPage /></blockroutefromloggedinuser>},
+            {path:"dashboard",element:  
+            <ProtectedRoute><DashboardPage /></ProtectedRoute>},
+
            
         ]
     }
 ])
+
