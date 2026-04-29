ccf28452823269b9ac07c121965fcb84bcef7f2c
Atul Yadav <atul99yadav99@gmail.com>
2026-04-29 16:47:28 +0530

    ui changes

--- frontend/src/components/pages/DashboardPage.jsx
+++ frontend/src/components/pages/DashboardPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import TryoutSection from '../sections/TryoutSection' 
+import TryoutSection from '../sections/TryoutSection'
 import Upload from '../sections/dashboardsection/Upload';
 
 import Sidebar from '../sections/dashboardsection/Sidebar';
@@ -8,8 +8,8 @@ import { Outlet } from 'react-router';
 function DashboardPage() {
   return (
     <div className='flex h-full w-full'>
-    <Sidebar/>
-    <div className="h-[80vh] overflow-y-auto pt-1"><Outlet/> </div>
+      <Sidebar />
+      <div className="h-[80vh] w-full overflow-y-auto pt-1"><Outlet /> </div>
     </div>
   )
 }
